f5c5d53ab0aac923545e1d222df90a9a2c4e4a46
Travis CI User <travis@example.org>
2017-04-20 22:03:24 +0000

    Documentation for diesel-rs/diesel@0c346f8

---
+++ implementors/core/convert/trait.Into.js
@@ -0,0 +1,16 @@
+(function() {var implementors = {};
+implementors["chrono"] = [];
+implementors["diesel"] = ["impl&lt;DB:&nbsp;<a class='trait' href='diesel/backend/trait.Backend.html' title='diesel::backend::Backend'>Backend</a>&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/convert/trait.Into.html' title='core::convert::Into'>Into</a>&lt;<a class='enum' href='https://doc.rust-lang.org/nightly/core/option/enum.Option.html' title='core::option::Option'>Option</a>&lt;<a class='struct' href='https://doc.rust-lang.org/nightly/alloc/boxed/struct.Box.html' title='alloc::boxed::Box'>Box</a>&lt;<a class='trait' href='diesel/query_builder/trait.QueryFragment.html' title='diesel::query_builder::QueryFragment'>QueryFragment</a>&lt;DB&gt;&gt;&gt;&gt; for <a class='struct' href='diesel/query_builder/where_clause/struct.NoWhereClause.html' title='diesel::query_builder::where_clause::NoWhereClause'>NoWhereClause</a>","impl&lt;'a, DB, Predicate&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/convert/trait.Into.html' title='core::convert::Into'>Into</a>&lt;<a class='enum' href='https://doc.rust-lang.org/nightly/core/option/enum.Option.html' title='core::option::Option'>Option</a>&lt;<a class='struct' href='https://doc.rust-lang.org/nightly/alloc/boxed/struct.Box.html' title='alloc::boxed::Box'>Box</a>&lt;<a class='trait' href='diesel/query_builder/trait.QueryFragment.html' title='diesel::query_builder::QueryFragment'>QueryFragment</a>&lt;DB&gt; + 'a&gt;&gt;&gt; for <a class='struct' href='diesel/query_builder/where_clause/struct.WhereClause.html' title='diesel::query_builder::where_clause::WhereClause'>WhereClause</a>&lt;Predicate&gt; <span class='where fmt-newline'>where DB: <a class='trait' href='diesel/backend/trait.Backend.html' title='diesel::backend::Backend'>Backend</a>,<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Predicate: <a class='trait' href='diesel/query_builder/trait.QueryFragment.html' title='diesel::query_builder::QueryFragment'>QueryFragment</a>&lt;DB&gt; + 'a</span>",];
+implementors["libc"] = [];
+implementors["num"] = [];
+implementors["quickcheck"] = [];
+implementors["regex_syntax"] = [];
+implementors["serde"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
